1ea7fb5db99c4c191206c07730df0b32c30dfe62
Tomek <thomas.jack321@yahoo.com>
2018-08-02 00:20:57 +0200

    Initial commit

---
+++ src/main/java/Example.java
@@ -0,0 +1,20 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home(){
+        return "Killer!" + "<p>" +Instant.now() + "</p>" + Math.PI;
+    }
+
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(Example.class, args);
+    }
+}
